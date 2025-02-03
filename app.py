00772d33fcdb43e8d50790aded399a3da0bf7e48
IdoBashari <ido1227@gmail.com>
2025-02-03 16:38:55 +0200

    Simplified app.py for Vercel deployment

--- app.py
+++ app.py
@@ -3,24 +3,21 @@ from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
+import jwt
 
 # Load environment variables
 load_dotenv()
 
-# Get MongoDB URI and JWT Secret
-mongo_uri = os.getenv("MONGO_URI")
-jwt_secret = os.getenv("JWT_SECRET_KEY")
-
 # Initialize Flask app
 app = Flask(__name__)
 
+# Configure app
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["MONGO_CONNECT_TIMEOUT_MS"] = 5000
+
 # Configure CORS
 CORS(app)
-
-# Configure app
-app.config["MONGO_URI"] = mongo_uri
-app.config["JWT_SECRET_KEY"] = jwt_secret
-app.config["MONGO_CONNECT_TIMEOUT_MS"] = 5000
 
 # Configure MongoDB
 mongo = PyMongo(app)
@@ -35,16 +32,9 @@ app.register_blueprint(user_routes)
 app.register_blueprint(rating_routes)
 app.register_blueprint(comment_routes)
 
-# Create indexes
-with app.app_context():
-    mongo.db.users.create_index("email", unique=True)
-    mongo.db.ratings.create_index(
-        [("user_id", 1), ("item_id", 1)],
-        unique=True,
-        name="unique_user_item_rating"
-    )
-
-# Root route
 @app.route('/')
-def index():
-    return {"status": "ok"}
+def home():
+   return {"message": "API is running"}
+
+# Export for vercel
+application = app
